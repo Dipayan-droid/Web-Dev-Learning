7ec4bf6ff25a2181b1fe40c6b46c6c9f9609208e
Dipayan-droid <mandaldipayan15@gmail.com>
2026-06-06 20:15:45 +0530

    Video 68

---
+++ video 69.js
@@ -0,0 +1,15 @@
+let a = 5
+function factorial(number){
+    let arr = Array.from(Array(number+1).keys())
+    let c = arr.slice(1,).reduce((a,b)=> a*b)
+    return c
+}
+function faFor(number){
+    let fac = 1;
+    for (let index = 1;index <= number;index++){
+        fac = fac * index
+    }
+    return fac
+}
+console.log(factorial(a))
+console.log(faFor(a))
